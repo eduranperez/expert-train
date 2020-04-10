e0b466c244bb4b913ed00d17cbf0707e7b3cee86
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2020-04-10 16:24:58 -0700

    simple template and linting

--- service-now-flask/src/service_now_proxy/app.py
+++ service-now-flask/src/service_now_proxy/app.py
@@ -1,11 +1,9 @@
 """flask app to communicate with service now."""
 import logging
-import json
 import os
 import pysnow
-import os   
 import sys
-from flask import Flask, request, Response, jsonify, session
+from flask import Flask, request, Response, jsonify
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 application = Flask(__name__)
@@ -13,9 +11,11 @@ SN_USERNAME = os.environ['SN_USERNAME']
 SN_PASSWORD = os.environ['SN_PASSWORD']
 SN_INSTANCE_URL = os.environ['SN_INSTANCE_URL']
 
+
 def health():
     """Simple health check."""
     return Response(status=200)
+
 
 def needits():
     """Return all needits."""
@@ -25,12 +25,34 @@ def needits():
         instance=SN_INSTANCE_URL,
         user=SN_USERNAME,
         password=SN_PASSWORD) 
-    needit = s.resource(api_path='/table/x_58872_needit_needit') 
-    record = needit.get(query={'number':text})
-    if record:
-        return jsonify(record.one()), 200
-    else:
-        return jsonify({'message': 'Record not found'})
+    needits = s.resource(api_path='/table/x_58872_needit_needit')
+    needit = needits.get(query={'number': text}).one_or_none()
+    msg = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ('*Incident Found.*' if needit else '*No records found.*')
+                }
+            },
+            {
+              "type": "actions",
+              "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Reply to review",
+                        "emoji": False
+                    }
+                }
+              ]
+            }
+        ]
+    }
+    return jsonify(msg), 200
+
 
 def slack_action():
     """Log slack action."""
@@ -41,10 +63,11 @@ def slack_action():
     else:
         return Response(status=200)
 
+
 def create_app():
     """Initialize app."""
     app = Flask(__name__)
     app.add_url_rule('/health', view_func=health, methods=['GET'])
-    app.add_url_rule('/needits', view_func=needits, methods=['GET', 'POST'])
-    app.add_url_rule('/slack', view_func=slack_action, methods=['GET','POST'])
+    app.add_url_rule('/needits', view_func=needits, methods=['POST'])
+    app.add_url_rule('/slack', view_func=slack_action, methods=['POST'])
     return app
