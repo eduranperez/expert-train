b377d84ae24e6d5cd73ff3c4e79510d43a898380
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2021-02-05 16:24:35 -0800

    offline development with lambda and dynamo

---
+++ offline-development-api/get-all/app.js
@@ -0,0 +1,37 @@
+const dynamoDb = require('aws-sdk/clients/dynamodb');
+exports.lambdaHandler = async (event, context) => {
+    try {
+        const params = {
+            TableName: 'items',
+        };
+        console.log('hi world');
+        console.log(event);
+        const docClient = exports.getDynamoDbDocumentClient();
+        
+        const response = await docClient.scan(params).promise();
+
+        return {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                items: response
+            })
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            'statusCode': 500
+        }
+    }
+};
+
+exports.getDynamoDbDocumentClient = () => {
+    console.log("Connecting to DB....");
+    console.log(process.env.USE_LOCAL_DYNAMODB);
+    if (process.env.USE_LOCAL_DYNAMODB == "true") {
+        console.log('connecting to local instance');
+        return new dynamoDb.DocumentClient({'endpoint': 'http://dynamo-local:8000'});
+    } else {
+        console.log('Connecting to cloud instance');
+        return new dynamoDb.DocumentClient();
+    }
+}
